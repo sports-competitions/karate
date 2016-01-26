106a3e9af9b9c34310e9c01d7cb3bd4bb5a20338
SobchenkoI <sobchenko.ihor@gmail.com>
2016-01-26 14:47:05 +0200

    Changing in routs.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   enum sex: [ :male, :female ]
   enum role: [ :admin, :moderator, :user ]
 
