aabf0e05840323bd8ebc06a84892bcc1ac6c07ed
SobchenkoI <sobchenko.ihor@gmail.com>
2016-01-26 15:02:41 +0200

    Changing migrations BUGs

--- db/migrate/20160126124008_add_devise_to_users.rb
+++ db/migrate/20160126124008_add_devise_to_users.rb
@@ -1,8 +1,13 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
+
+    change_column_null :users, :email, false
+    change_column_default :users, :email, from: "", to: ""
+
+
     change_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      # t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
