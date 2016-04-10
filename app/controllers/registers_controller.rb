9611fe361d4d0bcd5cfc1cc1d61d5b980766379a
rowen92 <maksym.pakhn@gmail.com>
2016-04-10 20:19:02 +0300

    fixed bugs

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -2,7 +2,7 @@ class RegistersController < ApplicationController
   before_action :authenticate_registrator!
   before_action :set_event
   before_action :set_register, only: [:show, :edit, :update, :destroy]
-  before_action :set_sportsmans, only: [:edit, :update]
+  before_action :set_people, only: [:new, :edit, :update]
 
   # GET /registers
   # GET /registers.json
@@ -13,7 +13,7 @@ class RegistersController < ApplicationController
   # GET /registers/1
   # GET /registers/1.json
   def show
-    @sportsmans = @register.people
+    @people = @register.people
   end
 
   # GET /registers/new
@@ -30,12 +30,9 @@ class RegistersController < ApplicationController
   def create
     @register = @event.registers.build(register_params)
     @register.registrator = current_registrator
-    # @people = Person.where(kind: "sportsman").where(registrator: current_registrator)
-    # @people << Person.find(params[:person_id])
 
     respond_to do |format|
       if @register.save
-        # @register.people << @people
         format.html { redirect_to event_register_url(@event, @register), notice: 'Register was successfully created.' }
         format.json { render :show, status: :created, location: event_register_url(@event, @register) }
       else
@@ -75,8 +72,8 @@ class RegistersController < ApplicationController
       @event = Event.find(params[:event_id])
     end
 
-    def set_sportsmans
-      @sportsmans = Person.where(kind: "sportsman").where(registrator: current_registrator)
+    def set_people
+      @people = Person.where(kind: "sportsman").where(registrator: current_registrator)
     end
 
     # Use callbacks to share common setup or constraints between actions.
