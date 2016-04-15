0c176f4bc8f60397c2b10378089a6ce09c5a6a9c
rowen92 <maksym.pakhn@gmail.com>
2016-04-15 13:47:03 +0300

    little translation

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -4,36 +4,28 @@ class RegistersController < ApplicationController
   before_action :set_register, only: [:show, :edit, :update, :destroy]
   before_action :set_people, only: [:new, :edit, :update]
 
-  # GET /registers
-  # GET /registers.json
   def index
     @registers = @event.registers
   end
 
-  # GET /registers/1
-  # GET /registers/1.json
   def show
     @people = @register.people
   end
 
-  # GET /registers/new
   def new
     @register = @event.registers.build
   end
 
-  # GET /registers/1/edit
   def edit
   end
 
-  # POST /registers
-  # POST /registers.json
   def create
     @register = @event.registers.build(register_params)
     @register.registrator = current_registrator
 
     respond_to do |format|
       if @register.save
-        format.html { redirect_to event_register_url(@event, @register), notice: 'Register was successfully created.' }
+        format.html { redirect_to event_register_url(@event, @register), notice: 'Заявка успішно подана' }
         format.json { render :show, status: :created, location: event_register_url(@event, @register) }
       else
         format.html { render :new }
@@ -42,12 +34,10 @@ class RegistersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /registers/1
-  # PATCH/PUT /registers/1.json
   def update
     respond_to do |format|
       if @register.update(register_params)
-        format.html { redirect_to event_register_url(@event, @register), notice: 'Register was successfully updated.' }
+        format.html { redirect_to event_register_url(@event, @register), notice: 'Заявка оновлена' }
         format.json { render :show, status: :ok, location: event_register_url(@event, @register) }
       else
         format.html { render :edit }
@@ -56,12 +46,10 @@ class RegistersController < ApplicationController
     end
   end
 
-  # DELETE /registers/1
-  # DELETE /registers/1.json
   def destroy
     @register.destroy
     respond_to do |format|
-      format.html { redirect_to event_registers_url, notice: 'Register was successfully destroyed.' }
+      format.html { redirect_to event_registers_url, notice: 'Заявка видалена' }
       format.json { head :no_content }
     end
   end
@@ -76,12 +64,10 @@ class RegistersController < ApplicationController
       @people = Person.where(kind: "sportsman").where(registrator: current_registrator)
     end
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_register
       @register = @event.registers.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def register_params
       params.require(:register).permit(:event_id, :name, person_ids: [])
     end
